d2993df9963e65256b8cb1c155fad7c6033aada9
vidhan gour <vidhangour20@gmail.com>
2024-04-06 03:05:36 -0700

    hh

--- src/utils/Apiroutes.js
+++ src/utils/Apiroutes.js
@@ -1,13 +1,13 @@
 export const host = process.env.REACT_APP_API_KEY;
 console.log("host is ",process.env.REACT_APP_API_KEY)
-export const allcategiryRoute = `${host}/api/v1/course/showAllCategories`;
+export const allcategiryRoute = `${host}/course/showAllCategories`;
 
-export const signUp =`${host}/api/v1/auth/signup`;
-export const sendOtp = `${host}/api/v1/auth/sendOtp`;
-export const resetPasswordToken = `${host}/api/v1/auth/reset-password-token`;
-export const resetPassword = `${host}/api/v1/auth/reset-password`;
-export const signup = `${host}/api/v1/auth/signup`;
-export const login =`${host}/api/v1/auth/login`;
+export const signUp =`${host}/auth/signup`;
+export const sendOtp = `${host}/auth/sendOtp`;
+export const resetPasswordToken = `${host}/auth/reset-password-token`;
+export const resetPassword = `${host}/auth/reset-password`;
+export const signup = `${host}/auth/signup`;
+export const login =`${host}/auth/login`;
 
 
 
